74c6b95a343b76433ba4f01757d64d1d96279fc7
Nasher3296 <ignaciofernandez3296@gmail.com>
2025-10-29 22:40:38 -0300

    fix: Fix ctx race condition at httplogger flush

--- pkg/logs/httplogger.go
+++ pkg/logs/httplogger.go
@@ -21,24 +21,23 @@ func NewHttpLogger(domain string) *HttpLogger {
 }
 
 func (l *HttpLogger) Flush(ctx context.Context, log *logger.Log) error {
-	go func() {
-		url := fmt.Sprintf("%s/logs", l.domain)
+	url := fmt.Sprintf("%s/logs", l.domain)
 
-		data, _ := json.Marshal(log)
+	data, _ := json.Marshal(log)
 
-		req, err := http.NewRequestWithContext(ctx, "POST", url, bytes.NewBuffer(data))
-		if err != nil {
-			fmt.Println("Error creating request: ", err)
-			return
-		}
+	req, err := http.NewRequestWithContext(ctx, "POST", url, bytes.NewBuffer(data))
+	if err != nil {
+		fmt.Println("Error creating request: ", err)
+		return err
+	}
 
-		req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Content-Type", "application/json")
 
-		_, err = http.DefaultClient.Do(req)
-		if err != nil {
-			fmt.Println("Error publishing event: ", err)
-		}
+	_, err = http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println("Error creating log: ", err)
+		return err
+	}
 
-	}()
 	return nil
 }
